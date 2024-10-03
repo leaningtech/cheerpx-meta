e0cbd1b559f207fe36e2bcefd5e38c35f825c4d2
Alessandro Pignotti <a.pignotti@sssup.it>
2024-10-03 22:13:14 +0200

    Restructure how APIs are exported from NPM packages

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 const version="0.8.7"
-export default (await import(`https://cxrtnc.leaningtech.com/${version}/cx.esm.js`)).default;
+const CheerpX = await import(`https://cxrtnc.leaningtech.com/${version}/cx.esm.js`);
+export const Linux = CheerpX.Linux;
+export const DataDevice = CheerpX.DataDevice;
+export const HttpBytesDevice = CheerpX.HttpBytesDevice;
+export const CloudDevice = CheerpX.CloudDevice;
+export const GitHubDevice = CheerpX.GitHubDevice;
+export const IDBDevice = CheerpX.IDBDevice;
+export const WebDevice = CheerpX.WebDevice;
+export const DataDevice = CheerpX.DataDevice;
+export const OverlayDevice = CheerpX.OverlayDevice;
+export const System = CheerpX.System;
