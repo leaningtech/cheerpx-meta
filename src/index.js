4e5f647e90a11b9bded70c5aee7a318ecc55b857
Alessandro Pignotti <a.pignotti@sssup.it>
2024-10-03 13:07:57 +0200

    Basic working support

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
-import { Linux, HttpBytesDevice, CloudDevice, GitHubDevice, IDBDevice, WebDevice, DataDevice, OverlayDevice, System } from 'https://cxrtnc.leaningtech.com/0.8.4/cx.esm.js';
-debugger;
-export default { Linux, HttpBytesDevice, CloudDevice, GitHubDevice, IDBDevice, WebDevice, DataDevice, OverlayDevice, System };
+import CheerpX from 'https://cxrtnc.leaningtech.com/0.8.4/cx.esm.js';
+export default CheerpX;
