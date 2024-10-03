32b22efd92d15a9e23ae3549f9ab707dd71305c5
Alessandro Pignotti <a.pignotti@sssup.it>
2024-10-03 13:55:40 +0200

    Prefer lowercase variable names

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-const VERSION="0.8.4"
-export default (await import(`https://cxrtnc.leaningtech.com/${VERSION}/cx.esm.js`)).default;
+const version="0.8.4"
+export default (await import(`https://cxrtnc.leaningtech.com/${version}/cx.esm.js`)).default;
